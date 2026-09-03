5616f25ac4aafdf408ddf078c551eb9a87375ea5
Mike Sowa <26012623@uepg.br>
2026-09-03 03:06:25 -0300

    att
    att

--- jogo/porta.gd
+++ jogo/porta.gd
@@ -1,11 +1,33 @@
 extends Area2D
 
 const FILE_BEGIN = "res://rooms/sala"
+var trocando_sala: bool = false
 
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() + 1
-		
-		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
-		get_tree().change_scene_to_file(next_level_path)
+
+func _on_body_entered(body: Node2D) -> void:
+	if trocando_sala:
+		return
+
+	if not body.is_in_group("Player") and not body.is_in_group("jogador"):
+		return
+
+	var cena_atual: String = get_tree().current_scene.scene_file_path
+	var nome_sala: String = cena_atual.get_file().get_basename()
+	var numero_atual: int = nome_sala.trim_prefix("sala").to_int()
+	var proxima_sala: String = FILE_BEGIN + str(numero_atual + 1) + ".tscn"
+
+	if not ResourceLoader.exists(proxima_sala):
+		push_error("A próxima sala não existe: " + proxima_sala)
+		return
+
+	trocando_sala = true
+	set_deferred("monitoring", false)
+	call_deferred("_trocar_para_proxima_sala", proxima_sala)
+
+
+func _trocar_para_proxima_sala(caminho: String) -> void:
+	var erro: Error = get_tree().change_scene_to_file(caminho)
+	if erro != OK:
+		trocando_sala = false
+		monitoring = true
+		push_error("Não foi possível abrir a sala: " + caminho)
